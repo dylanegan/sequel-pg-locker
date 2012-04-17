5f88d278d56e855a4bc8f49f5cf1c18d4d3a9aeb
Dylan Egan <dylanegan@gmail.com>
2012-04-17 15:28:20 -0400

    Ensure tests pass. :v:

--- spec/sequel-pg-locker_spec.rb
+++ spec/sequel-pg-locker_spec.rb
@@ -2,27 +2,23 @@ require 'minitest/autorun'
 
 require "sequel-pg-locker"
 
-LOCK_ID = rand(1_000_000)
+ENV['DATABASE_URL'] ||= "postgres://localhost/sequel_pg_locker_test"
 
 describe Sequel::PG::Locker do
   subject { Sequel::PG::Locker }
 
-  before do
-    ENV['DATABASE_URL'] = "postgres://localhost/sequel_pg_locker_test"
-  end
-
   describe ".lock" do
-    before do
-      subject.instance_variable_set(:@connection, nil)
-    end
-
     it "should provide a lock" do
-      assert subject.lock(LOCK_ID)
+      id = rand(1_000_000)
+      subject.instance_variable_set(:@db, nil)
+      assert subject.lock(id)
     end
 
     it "should not allow further locking" do
-      subject.lock(LOCK_ID)
-      assert !subject.lock(LOCK_ID)
+      id = rand(1_000_000)
+      assert subject.lock(id)
+      subject.instance_variable_set(:@db, nil)
+      assert !subject.lock(id)
     end
   end
 end
